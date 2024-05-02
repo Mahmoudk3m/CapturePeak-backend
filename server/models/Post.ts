033994f7dc1c7dadafc0cb0887ea87cfffa72fdb
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2024-05-02 19:53:51 +0300

    feat: Add Post and User models

---
+++ server/models/Post.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMedia extends Document {
+  title: string;
+  description: string;
+  path: string;
+  likes: number;
+  createdAt: Date;
+  authorId: Schema.Types.ObjectId;
+}
+
+const MediaSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: false },
+  path: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  authorId: { type: Schema.Types.ObjectId, ref: "User" }
+});
+
+// Indexes improve the efficiency of search operations.
+MediaSchema.index({ title: "text", description: "text" });
+
+export default mongoose.model<IMedia>("Media", MediaSchema);
